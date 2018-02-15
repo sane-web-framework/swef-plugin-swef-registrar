f7f270cbb6da8c27ae714f0635d260d316f72951
Mark Page <mark.page@whitelamp.com>
2018-02-15 15:45:29 +0000

    Initial commit

---
+++ app/config/static/Swef/SwefRegistrar.define.php
@@ -0,0 +1,15 @@
+<?php
+
+
+// Stored procedures
+define ( 'swefregistrar_call_may_self_reg',  'swefRegistrarMaySelfRegister'         );
+
+
+
+// Other tokens
+
+// Files
+define ( 'swefregistrar_file_dash',      __DIR__.'/SwefRegistrar.dash.html'     );
+define ( 'swefregistrar_file_password',  __DIR__.'/SwefRegistrar.password.html' );
+
+?>
---
+++ app/config/static/Swef/SwefRegistrar.require_once.php
@@ -0,0 +1,15 @@
+<?php
+
+// SYSTEM CONSTANTS
+require_once SWEF_CONFIG_PATH.'/static/Swef/SwefRegistrar.defines.php';
+
+
+// CONFIGURABLE CONSTANTS
+require_once SWEF_CONFIG_PATH.'/user/Swef/SwefRegistrar.config.php';
+
+
+// PLUGINS
+require_once SWEF_DIR_PLUGIN.'/Swef/SwefRegistrar.php';
+
+
+?>
